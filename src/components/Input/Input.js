c65e7d3c175bd970c622aa7f642d8fb3d0e5736f
Callasaurus <c.sullivandrage@outlook.com>
2022-11-24 14:28:00 +0000

    pushing css

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -72,15 +72,15 @@ export function Input(props) {
             <div className="exitButtonDiv">
                 <button className="exitButton" onClick={props.visibility}>X</button>
             </div>
-                <input placeholder="Title" onChange={handleTitle}></input>
+                <input placeholder="Title of the resource" onChange={handleTitle}></input>
                 
-                <input placeholder="Definition" onChange={handleDefinition}></input>
+                <input placeholder="Definition of the resouce topic" onChange={handleDefinition}></input>
                 
-                <input placeholder="Example" onChange={handleExample}></input>
+                <input placeholder="Example (image url)" onChange={handleExample}></input>
                 
-                <input placeholder="Links" onChange={handleLinks}></input>
+                <input placeholder="Links (url of helpful website(s))" onChange={handleLinks}></input>
                
-                <input default="888" placeholder="Week" onChange={handleWeek}></input>
+                <input placeholder="Week content covered (enter a number)" onChange={handleWeek}></input>
                
                 <button className="addButton" onClick={createObject}>Add</button>
             
@@ -94,17 +94,17 @@ export function Input(props) {
                 <button className="exitButton"onClick={props.visibility}>X</button>
             </div>
 
-                <input placeholder="Title in english" onChange={handleEnglishTitle}></input>
+                <input placeholder="Title of the resource in english" onChange={handleEnglishTitle}></input>
                 
-                <input placeholder="Title" onChange={handleTitle}></input>           
+                <input placeholder="Title of the resource in local language" onChange={handleTitle}></input>           
                 
-                <input placeholder="Definition" onChange={handleDefinition}></input>
+                <input placeholder="Definition of the resource topic" onChange={handleDefinition}></input>
                 
-                <input placeholder="Example" onChange={handleExample}></input>
+                <input placeholder="Example (image url)" onChange={handleExample}></input>
                 
-                <input placeholder="Links" onChange={handleLinks}></input>
+                <input placeholder="Links (url of helpful website(s))" onChange={handleLinks}></input>
                 
-                <input default="888" placeholder="Week" onChange={handleWeek}></input>
+                <input placeholder="Week content covered (enter a number)" onChange={handleWeek}></input>
                 
                 <button className="addButton" onClick={createObject}>Add</button>
             </div>
